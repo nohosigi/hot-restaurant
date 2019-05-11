ee12493d4b7d8d04170a7900ce38405babaac0eb
jmeyer <jmeyer@sellozo.com>
2019-05-11 12:54:41 -0500

    functional backend

--- controllers/api.js
+++ controllers/api.js
@@ -1,57 +1,45 @@
-tables = [
+let tables = [
     {
     "customerName": "tyrion lannister",
     "phoneNumber": "9999999999",
     "customerEmail": "tl@money.com",
     "customerID": "1"
     }
-    ]
+];
 
-    waitlist = [
-        [
-            {
-            "customerName": "",
-            "phoneNumber": "",
-            "customerEmail": "",
-            "customerID": ""
-            },
-            {
-            "customerName": "",
-            "phoneNumber": "",
-            "customerEmail": "",
-            "customerID": ""
-            },
-            {
-            "customerName": "Samuel",
-            "phoneNumber": "Fillingim",
-            "customerEmail": "mememe@me.com",
-            "customerID": "1"
-            }
-            ]
-    ]
+let waitlist = [
+        {
+        "customerName": "",
+        "phoneNumber": "",
+        "customerEmail": "",
+        "customerID": ""
+        },
+        {
+        "customerName": "",
+        "phoneNumber": "",
+        "customerEmail": "",
+        "customerID": ""
+        },
+        {
+        "customerName": "Samuel",
+        "phoneNumber": "Fillingim",
+        "customerEmail": "mememe@me.com",
+        "customerID": "1"
+        }
+];
 
 var ApiController = {
     waitlist: function(req,res){
-        console.log(req);
-    res.json(waitlist);
-},
-
+        res.json(waitlist);
+    },
     
     getTables: function (req,res){
-        console.log(req);
         res.json(tables);
     },
 
     postTables: function (req,res){
-        console.log(req.body);
-        var dummy = {
-            "customerName": "tyrion lannister",
-            "phoneNumber": "9999999999",
-            "customerEmail": "tl@money.com",
-            "customerID": "1"
-            };
-
-        tables.push(req.body); res.json(req.body);
+        tables.push(req.body);
+        res.json(req.body);
     },
     
     clear: function(req,res){
