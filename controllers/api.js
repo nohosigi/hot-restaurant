ee882be67cc1b8f765716e563d1f011adb4ddce7
nate pearson <nate913@gmail.com>
2019-05-11 12:51:24 -0500

    hookin stuff up

--- controllers/api.js
+++ controllers/api.js
@@ -1,18 +1,57 @@
+tables = [
+    {
+    "customerName": "tyrion lannister",
+    "phoneNumber": "9999999999",
+    "customerEmail": "tl@money.com",
+    "customerID": "1"
+    }
+    ]
+
+    waitlist = [
+        [
+            {
+            "customerName": "",
+            "phoneNumber": "",
+            "customerEmail": "",
+            "customerID": ""
+            },
+            {
+            "customerName": "",
+            "phoneNumber": "",
+            "customerEmail": "",
+            "customerID": ""
+            },
+            {
+            "customerName": "Samuel",
+            "phoneNumber": "Fillingim",
+            "customerEmail": "mememe@me.com",
+            "customerID": "1"
+            }
+            ]
+    ]
+
 var ApiController = {
     waitlist: function(req,res){
         console.log(req);
-  res.send("waiting list");
+    res.json(waitlist);
 },
 
     
     getTables: function (req,res){
         console.log(req);
-        res.send("avalible tables");
+        res.json(tables);
     },
 
     postTables: function (req,res){
-        console.log(req);
-        res.send("create tables");
+        console.log(req.body);
+        var dummy = {
+            "customerName": "tyrion lannister",
+            "phoneNumber": "9999999999",
+            "customerEmail": "tl@money.com",
+            "customerID": "1"
+            };
+
+        tables.push(req.body); res.json(req.body);
     },
     
     clear: function(req,res){
